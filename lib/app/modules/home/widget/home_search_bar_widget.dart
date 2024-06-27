9e54d15ab7904dc538c16d25a92a882372e04d3e
wcz0 <a@wcz0.com>
2024-06-27 23:18:34 +0800

    feat✨:

--- lib/app/modules/home/widget/home_search_bar_widget.dart
+++ lib/app/modules/home/widget/home_search_bar_widget.dart
@@ -10,42 +10,56 @@ class HomeSearchBarWidget extends StatelessWidget with BaseWidgetMixin {
   @override
   Widget body(BuildContext context) {
     return BrnAppBar(
-      leading: const Center(
-        child: Text(
-          '电商',
-          style: TextStyle(
-            fontSize: 14.0,
-          ),
-        ),
-      ),
-      title: Container(
-          padding: const EdgeInsets.all(16.0),
-          child: GestureDetector(
-            onTap: () {
-              Get.toNamed(Routes.homeSearch);
-            },
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: const Row(
-                children: [
-                  Icon(Icons.search),
-                  SizedBox(width: 8.0),
-                  Text(
-                    '搜索商品',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ],
+      leading: Container(
+          color: const Color.fromARGB(255, 247, 96, 85),
+          child: const Center(
+            child: Text(
+              '电商',
+              style: TextStyle(
+                fontSize: 14.0,
               ),
             ),
           )),
+      title: Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 247, 96, 85),
+              Color.fromARGB(255, 255, 153, 102),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: GestureDetector(
+          onTap: () {
+            Get.toNamed(Routes.homeSearch);
+          },
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: const Row(
+              children: [
+                Icon(Icons.search),
+                SizedBox(width: 8.0),
+                Text(
+                  '搜索商品',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       titleAlignment: Alignment.centerLeft,
-      backgroundColor: Color.fromARGB(255, 247, 96, 85),
+      // backgroundColor: Color.fromARGB(255, 247, 96, 85),
+      backgroundColor: Colors.transparent,
     );
   }
 }
