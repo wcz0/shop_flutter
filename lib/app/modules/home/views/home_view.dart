af792a700e7c02ce7088b0a82b2514a8cf258f06
wcz0 <a@wcz0.com>
2024-07-01 22:36:21 +0800

    feat✨:

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_swiper_view/flutter_swiper_view.dart';
 import 'package:shop_flutter/app/core/base/base_view.dart';
 import 'package:shop_flutter/app/modules/home/controllers/home_controller.dart';
 import 'package:shop_flutter/app/modules/home/widget/home_search_bar_widget.dart';
+import 'package:shop_flutter/gen/assets.gen.dart';
 
 class HomeView extends BaseView<HomeController> {
   HomeView({super.key});
@@ -15,12 +16,58 @@ class HomeView extends BaseView<HomeController> {
     return Column(
       children: [
         HomeSearchBarWidget(),
-        swiper(context),
+        // swiper(context),
+        category(context),
       ],
     );
   }
 
   Widget swiper(BuildContext context) {
     return Swiper(itemCount: 1);
+  }
+
+  Widget category(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+          children: [
+            {'image': Assets.images.all.image(), 'label': '全部商品'},
+            {'image': Assets.images.coupon.image(), 'label': '领取优惠券'},
+            {'image': Assets.images.gift.image(), 'label': '精品推荐'},
+            {'image': Assets.images.news.image(), 'label': '新闻资讯'},
+            {'image': Assets.images.bargain.image(), 'label': '助力砍价'},
+            {'image': Assets.images.seckill.image(), 'label': '限时秒杀'},
+            {'image': Assets.images.point.image(), 'label': '积分商城'},
+            {'image': Assets.images.rebate.image(), 'label': '团长返佣'},
+            {'image': Assets.images.footprint.image(), 'label': '用户足迹'},
+            {'image': Assets.images.prizeWheel.image(), 'label': '幸运转盘'},
+          ].map((item) {
+            return Column(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    item['image'] as String,
+                    fit: BoxFit.cover,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                ),
+                const SizedBox(height: 4.0), // 图片和文字之间的间距
+                Center(
+                  child: Text(
+                    item['label'].toString(),
+                    style: const TextStyle(fontSize: 12.0),
+                  ),
+                ),
+              ],
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -7,11 +7,140 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
+import 'package:flutter/widgets.dart';
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/all.png
+  AssetGenImage get all => const AssetGenImage('assets/images/all.png');
+
+  /// File path: assets/images/bargain.png
+  AssetGenImage get bargain => const AssetGenImage('assets/images/bargain.png');
+
+  /// File path: assets/images/coupon.png
+  AssetGenImage get coupon => const AssetGenImage('assets/images/coupon.png');
+
+  /// File path: assets/images/footprint.png
+  AssetGenImage get footprint =>
+      const AssetGenImage('assets/images/footprint.png');
+
+  /// File path: assets/images/gift.png
+  AssetGenImage get gift => const AssetGenImage('assets/images/gift.png');
+
+  /// File path: assets/images/news.png
+  AssetGenImage get news => const AssetGenImage('assets/images/news.png');
+
+  /// File path: assets/images/point.png
+  AssetGenImage get point => const AssetGenImage('assets/images/point.png');
+
+  /// File path: assets/images/prize_wheel.png
+  AssetGenImage get prizeWheel =>
+      const AssetGenImage('assets/images/prize_wheel.png');
+
+  /// File path: assets/images/rebate.png
+  AssetGenImage get rebate => const AssetGenImage('assets/images/rebate.png');
+
+  /// File path: assets/images/seckill.png
+  AssetGenImage get seckill => const AssetGenImage('assets/images/seckill.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        all,
+        bargain,
+        coupon,
+        footprint,
+        gift,
+        news,
+        point,
+        prizeWheel,
+        rebate,
+        seckill
+      ];
+}
+
 class Assets {
   Assets._();
 
   static const String aEnv = '.env';
+  static const $AssetsImagesGen images = $AssetsImagesGen();
 
   /// List of all assets
   static List<String> get values => [aEnv];
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size = null});
+
+  final String _assetName;
+
+  final Size? size;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
 }
